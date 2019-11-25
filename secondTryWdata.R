d6af529a63600c26867c0aff5fb5161d6a85e040
FREMY Enora <EF249002@intra.cea.fr>
2019-11-25 15:21:26 +0100

    reactiveValues, isolate and observe, plus comments

--- secondTryWdata.R
+++ secondTryWdata.R
@@ -1,26 +1,30 @@
 library(shiny)
 library(DAPAR)
-library(DAPARdata)
 
 # boxplot quanti c vs D
 # display tab 2 first column of feature
+
+utils::data(Exp1_R25_prot, package='DAPARdata') #load dataset Exp1_R25_prot from package DAPARdata
+obj <- Exp1_R25_prot
+feature <- Biobase::fData(obj)
 
 ui <- fluidPage(
   
   titlePanel("Test avec donnees Exp1_R25_prot"), 
   
   mainPanel(
+    actionButton('btn', 'toto'), # to update what's inside isolate()
     
-    plotOutput("plot"), 
+    plotOutput("plot"), # plot = boxplot
     
     column(3, sliderInput("row", "Choose row",
-                       min = 1, max = nrow(feature), value = c(0, 10))),
+                       min = 1, max = nrow(feature), value = c(1, 5))),
     column(3, sliderInput("col", "Choose column",
-                min = 1, max = ncol(feature), value = c(0, 10))),
+                min = 1, max = ncol(feature), value = c(0, 5))), # two slider bars to choose boundaries of row and column
     
-    column(6,textOutput("selected_var")),
+    column(6,textOutput("selected_var")), # text output
     
-    tableOutput('table')
+    tableOutput('table') # tab output
   )
 
 )
@@ -28,37 +32,46 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  #rv <- reactiveValues( # when value modified, modified everywhere
-  #  data = NULL
-  #)
   
-  data("Exp1_R25_prot")
-  #rv$obj <- Exp1_R25_prot
-  obj <- Exp1_R25_prot
-  quanti <- Biobase::exprs(obj)
-  dim(quanti) # 2384 6
-  feature <- Biobase::fData(obj)
-  dim(feature) # 2384 86
+  rv <- reactiveValues( # when value is modified, saved in rv$data and changed everywhere
+    data = NULL
+  )
   
-  output$plot <- renderPlot({
-    
-    #conds <- Biobase::pData(obj)[,"Condition"]
-    #DAPAR::boxPlotD(obj)
-    boxplot(quanti, main = "Condition C vs D in quantification data")
+  toto <- 0
+  
+  
+  output$plot <- renderPlot({ 
+    input$btn # display button toto to update the boxplot
+
+    isolate({ # all inside isolate() is unchaged until input$btn toto pushed
+      quanti <- Biobase::exprs(obj)[c(input$row[1]:input$row[2]),]
+      boxplot(quanti, main = "Condition C vs D in quantification data")
+    })
     
     })
   
   
+  observe({ # observe when input$row varies then only update functions which use input$row
+    input$row
+    
+    rv$data <- input$row[1] # overwrite rv$data with new input$row[1] value
+    
+  })
+  
+  
   output$selected_var <- renderText({ 
     
-    paste("You choose rows", input$row[1], "to", input$row[2], "and columns", 
-          input$col[1], "to", input$col[2], "of feature tab", sep = " ")
+    print(input$btn) # display in console
+    print(rv$data) # display in console
+    
+      paste("You choose rows", rv$data, "to", input$row[2], "and columns", 
+          input$col[1], "to", input$col[2], "of feature tab", sep = " ") # here, rv$data is input$row[1]
     
   })
   
   output$table <- renderTable({
     
-    feature[input$row[1]:input$row[2],input$col[1]:input$col[2]]
+    feature[input$row[1]:input$row[2],input$col[1]:input$col[2]] # display tab "feature" with boundaries of sliderInput
     #feature[1:3,1:3]
     
     })
